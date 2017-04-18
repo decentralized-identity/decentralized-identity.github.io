11f3292e712c580c79d3f55e480017f86c800ee1
Daniel Buchner <danieljb2@gmail.com>
2017-04-17 17:53:54 -0700

    Initial commit

---
+++ js/routing.js
@@ -0,0 +1,13 @@
+(function(){
+  var redirect = sessionStorage.redirect;
+  delete sessionStorage.redirect;
+  if (redirect && redirect != location.href) {
+    history.replaceState(null, null, redirect);
+    // REMOVE THIS - just showing the redirect route in the UI
+    document.documentElement.setAttribute('message', 'This page was redirected by 404.html, from the route: ' + redirect);
+  }
+  else {
+    // REMOVE THIS - just showing the redirect route in the UI
+    document.documentElement.setAttribute('message', 'This page was loaded directly from the index.html file');
+  }
+})();
