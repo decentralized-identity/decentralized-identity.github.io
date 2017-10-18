43c7f4ae5375175652470f91742599869adadab2
Christopher Van Wiemeersch <hearcomestreble@gmail.com>
2017-10-18 02:45:41 -0700

    fix back/forward navigation routing (fixes issue #1); fix broken document `<title>`s (fixes #4)

--- js/routing.js
+++ js/routing.js
@@ -1,38 +1,58 @@
-
-
-(function(){
-
+/* global ga */
+(function() {
   var titles = {
-    '/': 'Home',
+    '/': 'Decentralized Identity Foundation',
     '/working-groups': 'Working Groups',
     '/membership': 'Membership'
   };
 
-  window.routeUpdate = function routeUpdate(href, push) {
-    var title = titles[location.pathname];
-    document.title = 'DIF - ' + title;
-    if (push !== false) {
-      history.pushState(null, 'DIF - ' + title, href);
-      ga('set', {
-        page: location.pathname,
-        title: title
-      });
-      ga('send', 'pageview')
+  var routeUpdate = window.routeUpdate = function routeUpdate(href, push) {
+    if (!href) {
+      throw new Error('Must pass `href` as the first parameter to `routeUpdate`');
     }
-    document.body.setAttribute('path', location.pathname);   
+    href = href.replace(/.html$/, '');
+    var title = 'DIF - ' + (titles[href] || titles['/']);
+    var prevState = {
+      path: location.pathname,
+      title: document.title
+    };
+    document.title = title;
+    if (history.scrollRestoration !== 'auto') {
+      window.scrollTo(0, 0);
+    }
+    if (push === false) {
+      history.replaceState(prevState, null, href);
+    } else {
+      history.pushState(prevState, title, href);
+      if ('ga' in window) {
+        ga('set', {
+          page: location.pathname,
+          title: state.title
+        });
+        ga('send', 'pageview');
+      }
+    }
+    document.body.setAttribute('path', location.pathname);
+  };
+
+  var redirect = null;
+  try {
+    redirect = window.sessionStorage.redirect;
+    delete window.sessionStorage.redirect;
+  } catch (err) {
   }
-  
-  var redirect = sessionStorage.redirect;
-  delete sessionStorage.redirect;
-  if (redirect && redirect != location.href) {
-    history.replaceState(null, null, redirect);
+  if (redirect && redirect !== location.href) {
     routeUpdate(redirect, false);
     // REMOVE THIS - just showing the redirect route in the UI
     document.documentElement.setAttribute('message', 'This page was redirected by 404.html, from the route: ' + redirect);
   }
   else {
+    routeUpdate(location.pathname, true);
     // REMOVE THIS - just showing the redirect route in the UI
     document.documentElement.setAttribute('message', 'This page was loaded directly from the index.html file');
   }
 
+  window.onpopstate = function(e) {
+    routeUpdate(e.state.path, false);
+  };
 })();
