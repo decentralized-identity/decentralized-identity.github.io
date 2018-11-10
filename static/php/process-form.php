db715d5358e0d97249714ce8aedcd64fb03e7a3b
Daniel Buchner <danieljb2@gmail.com>
2018-11-10 14:10:55 -0800

    Push new site branch

---
+++ static/php/process-form.php
@@ -0,0 +1,19 @@
+<?php
+if (isset($_REQUEST['name'],$_REQUEST['email'])) {
+      
+    $name = $_REQUEST['name'];
+    $email = $_REQUEST['email'];
+    $message = $_REQUEST['message'];
+      
+    // Set your email address where you want to receive emails. 
+    $to = 'sage.kv4@gmail.com';
+      
+    $subject = 'Contact Request From Website';
+    $headers = "From: ".$name." <".$email."> \r\n";
+      
+    $send_email = mail($to,$subject,$message,$headers);
+      
+    echo ($send_email) ? 'success' : 'error';
+      
+}
+?>
