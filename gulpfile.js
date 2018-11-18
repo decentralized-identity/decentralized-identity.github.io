aefea9574fdd074843a474b8dbf22dc9e6efeb23
Daniel Buchner <danieljb2@gmail.com>
2018-11-18 07:22:06 -0800

    move site to templates and build process

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const nunjucksRender = require('gulp-nunjucks-render');
+const gulpSequence = require('gulp-sequence');
+const del = require('del');
+
+var assets = {
+  js: [
+    'js/jquery-3.2.1.min.js',
+    'js/jquery-migrate-3.0.0.min.js',
+    'plugins/bootstrap/js/popper.min.js',
+    'plugins/bootstrap/js/bootstrap.min.js',
+    'plugins/owl-carousel/js/owl.carousel.min.js',
+    'plugins/magnific/jquery.magnific-popup.min.js',
+    'js/custom.js'
+  ]
+};
+
+var structure = {
+  '': 'index.html',
+  'working-groups': [
+    'identifiers-names-discovery.html',
+    'storage-compute.html',
+  ]
+}
+
+gulp.task('assets', function() {
+  return gulp.src(assets.js)
+    .pipe(uglify())
+    .pipe(concat('base.js'))
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task('templates', function() {
+  return gulp.src('templates/pages/*.html')
+    .pipe(nunjucksRender({
+      path: ['templates', 'templates/partials', 'templates/pages']
+    }))
+    .pipe(gulp.dest('tmp'))
+});
+
+gulp.task('structure', function() {
+  for (let z in structure) {
+    (Array.isArray(structure[z]) ? structure[z] : [structure[z]]).forEach(file => {
+      gulp.src('tmp/' + file).pipe(gulp.dest(z));
+    });
+  }
+});
+
+gulp.task('cleanup', function() {
+  del('tmp/**');
+});
+
+gulp.task('build', gulpSequence('assets', 'templates', 'structure', 'cleanup'));
---
+++ node_modules/gulp-nunjucks-render/index.js
@@ -0,0 +1,84 @@
+'use strict';
+var _ = require('lodash');
+var PluginError = require('plugin-error');
+var replaceExtension = require('replace-ext');
+var through = require('through2');
+var nunjucks = require('nunjucks');
+
+var defaults = {
+    path: '.',
+    ext: '.html',
+    data: {},
+    inheritExtension: false,
+    envOptions: {
+      watch: false
+    },
+    manageEnv: null
+};
+
+module.exports = function (options) {
+  options = _.defaultsDeep(options || {}, defaults);
+  nunjucks.configure(options.envOptions);
+
+  if (!options.loaders) {
+    options.loaders = new nunjucks.FileSystemLoader(options.path);
+  }
+
+  var compile = new nunjucks.Environment(options.loaders, options.envOptions);
+
+  if (_.isFunction(options.manageEnv)) {
+    options.manageEnv.call(null, compile);
+  }
+
+  /*
+   * file = file
+   * cb   = callback function
+   */
+  return through.obj(function(file, enc, cb) {
+    var data = _.cloneDeep(options.data);
+
+    if (file.isNull()) {
+      this.push(file);
+      return cb();
+    }
+
+    if (file.data) {
+      data = _.merge(file.data, data);
+    }
+
+    if (file.isStream()) {
+      this.emit('error', new PluginError('gulp-nunjucks', 'Streaming not supported'));
+      return cb();
+    }
+
+    var _this = this;
+
+    var filePath = file.path;
+
+    try {
+      compile.renderString(file.contents.toString(), data, function (err, result) {
+        if (err) {
+          _this.emit('error', new PluginError('gulp-nunjucks', err, {fileName: filePath}));
+          return cb();
+        }
+        file.contents = new Buffer(result);
+        // Replace extension with mentioned/default extension
+        // only if inherit extension flag is not provided(truthy)
+        if (!options.inheritExtension) {
+          file.path = replaceExtension(filePath, options.ext);
+        }
+        _this.push(file);
+        cb();
+      });
+    } catch (err) {
+      _this.emit('error', new PluginError('gulp-nunjucks', err, {fileName: filePath}));
+      cb();
+    }
+  });
+};
+
+module.exports.setDefaults = function (options) {
+  defaults = _.defaultsDeep(options || {}, defaults);
+};
+
+module.exports.nunjucks = nunjucks;
