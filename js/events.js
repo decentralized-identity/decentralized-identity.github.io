1f19f10ef0d831d036b4989ed67280f1aeefb1d8
Daniel Buchner <danieljb2@gmail.com>
2019-07-05 10:43:15 -0700

    wip for event page

--- js/events.js
+++ js/events.js
@@ -19,37 +19,43 @@ async function getEvents(){
 }
 
 getEvents().then(events => {
-  event_list.innerHTML = events.sort((a,b) => {
+  var html = '';
+  var today = new Date();
+  events.sort((a,b) => {
       return new Date(a.startdate) - new Date(b.startdate);
-    }).map(event => {
-      let startDate = new Date(event.startdate);
-      let endDate = new Date(event.enddate);
-      let startMonth = startDate.toLocaleString('en-us', { month: 'short' });
-      let startMonthLong = startDate.toLocaleString('en-us', { month: 'long' });
-      let endMonth = endDate.toLocaleString('en-us', { month: 'short' });
-      let endMonthLong = endDate.toLocaleString('en-us', { month: 'long' });
-      let searchStrings = [
-          event.eventname,
-          event.location,
-          startMonthLong,
-          startDate.getFullYear(),
-          endMonthLong,
-          endDate.getFullYear()
-        ].map(s => String(s).toLowerCase()).join(' ');
+    })
+    .forEach(event => {
+      if (today <= new Date(event.enddate)) {
+        let startDate = new Date(event.startdate);
+        let endDate = new Date(event.enddate);
+        let startMonth = startDate.toLocaleString('en-us', { month: 'short' });
+        let startMonthLong = startDate.toLocaleString('en-us', { month: 'long' });
+        let endMonth = endDate.toLocaleString('en-us', { month: 'short' });
+        let endMonthLong = endDate.toLocaleString('en-us', { month: 'long' });
+        let searchStrings = [
+            event.eventname,
+            event.location,
+            startMonthLong,
+            startDate.getFullYear(),
+            endMonthLong,
+            endDate.getFullYear()
+          ].map(s => String(s).toLowerCase()).join(' ');
 
-      return `<li data-filter="${searchStrings}">
-                <div class="event-list-left">
-                  <a href="${event.link}" datestamp start-month="${startMonth}" start-day="${startDate.getDate()}" ${endMonth == startMonth ? '' : 'end-month="' + endMonth + '"'} end-day="${endDate.getDate()}">
-                    <img src="${ event.primaryimage ? 'https://drive.google.com/thumbnail?id=' + event.primaryimage.split('=')[1] : '' }"/>  
-                  </a>
-                </div>
-                <dl class="event-list-center">
-                  <dt><a href="${event.link}">${event.eventname}</a></dt>
-                  <dd class="event-location">${event.location}</dd>
-                  <dd>${event.description}</dd>
-                </dl>
-              </li>`
-    }).join();
+        html += `<li data-filter="${searchStrings}">
+          <div class="event-list-left">
+            <a href="${event.link}" datestamp start-month="${startMonth}" start-day="${startDate.getDate()}" ${endMonth == startMonth ? '' : 'end-month="' + endMonth + '"'} end-day="${endDate.getDate()}">
+              <img src="${ event.primaryimage ? 'https://drive.google.com/thumbnail?id=' + event.primaryimage.split('=')[1] : '' }"/>  
+            </a>
+          </div>
+          <dl class="event-list-center">
+            <dt><a href="${event.link}">${event.eventname}</a></dt>
+            <dd class="event-location">${event.location}</dd>
+            <dd>${event.description}</dd>
+          </dl>
+        </li>`;
+      }
+    });
+    event_list.innerHTML = html;
 });
 
 (function(){
