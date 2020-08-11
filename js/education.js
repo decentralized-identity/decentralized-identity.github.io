38df98dabe3d104b7c442465204981a89cd1517f
nembal <nembal@gmail.com>
2020-08-11 12:19:16 +0200

    edu blog post roll

--- js/education.js
+++ js/education.js
@@ -1,6 +1,6 @@
 (function(){
 
-  fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/decentralized-identity').then(res => res.json()).then(json => {
+  fetch('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Fdecentralized-identity%2Ftagged%2Feducation').then(res => res.json()).then(json => {
     blog_posts.innerHTML = json.items.slice(0, 3).map(post => {
       return `
         <div class="col-12 col-md-6 col-lg-4">
@@ -20,4 +20,19 @@
   }).catch(e => {
     console.log('Cannot load Medium feed', e);
   });
+
+  document.getElementById('newsletter_form').addEventListener('submit', function(e) {
+    try {
+      e.preventDefault();
+      var request = new XMLHttpRequest();
+      request.open('POST', 'https://docs.google.com/forms/d/1ne4T2ojHCxkqPuHq6hCUNLlOoTAPZwBqhK6GwJ5zywY/formResponse');
+      request.send(new FormData(this));
+      this.innerHTML = '<div class="inquiry-submitted">Thank you for signing up for our newsletter!<div>';
+      ga('send', 'event', 'Inquiry', 'submit', 'Newsletter');
+    }
+    catch (e) {
+      console.warn('Form submission error:', e);
+    }
+  });
+
 })()
