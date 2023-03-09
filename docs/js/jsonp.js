b948d069cd8b5f5b28d33520946f1c911dcf4092
paul-yellowpink <99773940+paulgrehan@users.noreply.github.com>
2023-03-09 12:41:59 +1000

    Member updates

--- docs/js/jsonp.js
+++ docs/js/jsonp.js
@@ -25,7 +25,7 @@
 
   function clearFunction(functionName) {
     // IE8 throws an exception when you try to delete a property on window
-    // http://stackoverflow.com/a/1824228/751089
+    // https://stackoverflow.com/a/1824228/751089
     try {
       delete window[functionName];
     } catch (e) {
